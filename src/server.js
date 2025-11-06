8b0ece2811e91a70fc48911d3742ef070130a1b1
Sapper <saniamykhailenko@gmail.com>
2025-11-06 17:54:38 +0200

    fix PORT

--- src/server.js
+++ src/server.js
@@ -9,7 +9,7 @@ import { errorHandler } from './middleware/errorHandler.js';
 import productsRouter from './routes/productsRoutes.js';
 
 const app = express();
-const PORT = process.env.PORT ?? 3030;
+const PORT = process.env.PORT ?? 3000;
 
 app.use(express.json());
 app.use(cors());
